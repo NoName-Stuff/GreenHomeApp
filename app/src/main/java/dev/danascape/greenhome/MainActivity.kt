638a6c17ef8a0dbf5eaa5253ea5a3a101addeefb
debz-g <debz.exe@gmail.com>
2023-04-20 03:08:22 +0530

    Added UI changes

--- app/src/main/java/dev/danascape/greenhome/MainActivity.kt
+++ app/src/main/java/dev/danascape/greenhome/MainActivity.kt
@@ -2,10 +2,19 @@ package dev.danascape.greenhome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import dev.danascape.greenhome.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        
+        binding.imgPlant.setOnClickListener {
+            Toast.makeText(this, "next page", Toast.LENGTH_SHORT).show()
+        }
+        
+        setContentView(binding.root)
     }
 }
