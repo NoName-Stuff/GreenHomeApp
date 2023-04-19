3f9f8f44b3c62812c43c71e9f1ce71153da3fee9
danascape <danascape@gmail.com>
2023-04-20 03:11:50 +0530

    GreenHome: Add plant activity
    Signed-off-by: danascape <danascape@gmail.com>

--- app/src/main/java/dev/danascape/greenhome/MainActivity.kt
+++ app/src/main/java/dev/danascape/greenhome/MainActivity.kt
@@ -1,9 +1,12 @@
 package dev.danascape.greenhome
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import dev.danascape.greenhome.databinding.ActivityMainBinding
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,9 +15,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         
         binding.imgPlant.setOnClickListener {
-            Toast.makeText(this, "next page", Toast.LENGTH_SHORT).show()
+            Intent(this, PlantActivity::class.java).also {
+                startActivity(it)
+            }
         }
         
         setContentView(binding.root)
+
     }
 }
