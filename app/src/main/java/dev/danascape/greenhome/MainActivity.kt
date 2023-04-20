4a77b3a93a4fb97e2c441b3b1301ca013ad87a05
danascape <danascape@gmail.com>
2023-04-20 11:50:30 +0530

    GreenHome: Update
    Signed-off-by: danascape <danascape@gmail.com>

--- app/src/main/java/dev/danascape/greenhome/MainActivity.kt
+++ app/src/main/java/dev/danascape/greenhome/MainActivity.kt
@@ -1,9 +1,6 @@
 package dev.danascape.greenhome
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import dev.danascape.greenhome.databinding.ActivityMainBinding
 
@@ -12,30 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        
-        binding.imgPlant.setOnClickListener {
-            Intent(this, PlantActivity::class.java).also {
-                startActivity(it)
-            }
-        }
-
-        val radius = 20f
-
 
         setContentView(binding.root)
 
-    }
-
-    fun blur(
-        viewToBlur: MainActivity = this,
-        radius: Float = 16f,
-        isAutoUpdate: Boolean = true,
-        hasFixedTransformationMatrix: Boolean = true,
-    ) {
-        binding.blrView.setupWith(binding.blrView)
-//            .setBlurAlgorithm(RenderScriptBlur(this.context))
-            .setBlurRadius(radius)
-            .setBlurAutoUpdate(isAutoUpdate)
-//            .setHasFixedTransformationMatrix(hasFixedTransformationMatrix)
     }
 }
