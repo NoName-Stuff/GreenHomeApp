e36c11633bd646c3c686e33c49347430f785adf4
danascape <danascape@gmail.com>
2023-04-20 11:47:37 +0530

    GreenHome: Update
    Signed-off-by: danascape <danascape@gmail.com>

--- app/src/main/java/dev/danascape/greenhome/MainActivity.kt
+++ app/src/main/java/dev/danascape/greenhome/MainActivity.kt
@@ -1,12 +1,11 @@
 package dev.danascape.greenhome
 
 import android.content.Intent
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import dev.danascape.greenhome.databinding.ActivityMainBinding
-import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -19,8 +18,24 @@ class MainActivity : AppCompatActivity() {
                 startActivity(it)
             }
         }
-        
+
+        val radius = 20f
+
+
         setContentView(binding.root)
 
+    }
+
+    fun blur(
+        viewToBlur: MainActivity = this,
+        radius: Float = 16f,
+        isAutoUpdate: Boolean = true,
+        hasFixedTransformationMatrix: Boolean = true,
+    ) {
+        binding.blrView.setupWith(binding.blrView)
+//            .setBlurAlgorithm(RenderScriptBlur(this.context))
+            .setBlurRadius(radius)
+            .setBlurAutoUpdate(isAutoUpdate)
+//            .setHasFixedTransformationMatrix(hasFixedTransformationMatrix)
     }
 }
